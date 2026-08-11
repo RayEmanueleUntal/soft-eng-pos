44e8ead1a3b740b304c6e83c85bb0b524d2d4fd9
Cary Ariola <carygomezariola@gmail.com>
2026-08-11 21:09:39 +0800

    added sample data at dashboards\products\data.ts

--- app/dashboard/products/data.ts
+++ app/dashboard/products/data.ts
@@ -1,5 +1,22 @@
 import { Product } from "./products"
 
 export const products: Product[] = [
-  
+  {
+    name: "Bolt A1",
+    category: "Fasteners",
+    threadType: "Metric",
+    material: "Steel",
+    size: "M8 x 20mm",
+    retailPrice: 2.5,
+    wholesalePrice: 1.8,
+  },
+  {
+    name: "Nut B2",
+    category: "Fasteners",
+    threadType: "Imperial",
+    material: "Brass",
+    size: "1/4 inch",
+    retailPrice: 1.2,
+    wholesalePrice: 0.9,
+  },
 ]
